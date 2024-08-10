c2b5d94a468e058d781f6f58235a5b0f2387f8be
Kaz Michael Karl <kazmichaelkarl@gmail.com>
2024-08-09 22:13:32 -0400

    Firsties!

---
+++ CruDzApplication.java
@@ -0,0 +1,13 @@
+package com.CRUDz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruDzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruDzApplication.class, args);
+	}
+
+}
